9b90738025d6ffbb785081e8ffc204b10f71de2d
Roger Guasch <rguasch@leadtech.com>
2022-10-14 15:35:22 +0200

    enable xdebug by default

--- php/bin/disable_xdebug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -i "s/^xdebug.start_with_request=yes/xdebug.start_with_request=no/g" /usr/local/etc/php/conf.d/xdebug.ini
-kill -USR2 1
--- php/bin/enable_xdebug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -i "s/^xdebug.start_with_request=no/xdebug.start_with_request=yes/g" /usr/local/etc/php/conf.d/xdebug.ini
-kill -USR2 1
